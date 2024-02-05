66e9091aba74fa13a9059e2339d5c0a38ada935e
dev.cappp <dev.cappp@gmail.com>
2024-02-05 23:16:22 +0900

    feat: submenu hover 시 css변경 추가작업중

--- js/header.js
+++ js/header.js
@@ -1,37 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var header = document.querySelector("header");
-  var headerHeight = header.offsetHeight;
-  var submenu = document.querySelector(".subMenu");
-  var searchButton = document.querySelector(".search");
-  var searchDown = document.getElementById("searchDown");
-  var closeBtn = document.querySelector(".closeBtn");
+  const menuItem = document.querySelector(".mainMenu > li");
+  const subMenu = document.querySelector(".subMenu");
+  const searchMenu = document.querySelector(".searchMenu");
+  const closeBtn = document.querySelector(".closeBtn");
 
-  searchButton.addEventListener("click", function () {
-    searchDown.style.display =
-      searchDown.style.display === "none" || searchDown.style.display === ""
-        ? "block"
-        : "none";
-
-    closeBtn.style.display =
-      closeBtn.style.display === "none" || closeBtn.style.display === ""
-        ? "block"
-        : "none";
+  menuItem.addEventListener("mouseover", function () {
+    subMenu.style.display = "block";
   });
 
-  closeBtn.addEventListener("click", function () {
-    searchDown.style.display = "none";
-    closeBtn.style.display = "none";
-  });
 
-  window.addEventListener("scroll", function () {
-    var scrollPosition = window.scrollY;
 
-    // 스크롤 위치가 header의 하단을 넘어가면
-    if (scrollPosition > headerHeight) {
-      submenu.style.top = "0";
-    } else {
-      // 스크롤 위치가 header의 하단 이하면
-      submenu.style.top = headerHeight - scrollPosition + "px";
-    }
-  });
+ 
 });
