94a0b30d5a789b7f131b7a0ea77367b5ce1b0d51
dev.cappp <dev.cappp@gmail.com>
2024-02-08 23:20:36 +0900

    design: submenu a 태그 hover작업 제외하고 완성

--- js/header.js
+++ js/header.js
@@ -5,42 +5,28 @@ document.addEventListener("DOMContentLoaded", function () {
   const subMenu = document.querySelector(".subMenu");
   // var submenu = document.querySelector(".subMenu");
   var searchButton = document.querySelector(".search");
+  // var searchDown = document.querySelector(".searchMenu");
   var searchDown = document.getElementById("searchDown");
   var closeBtn = document.querySelector(".closeBtn");
 
   menuItem.addEventListener("mouseover", function () {
+    subMenu.style.height = "378px";
     subMenu.style.display = "block";
   });
 
-  // searchButton.addEventListener("click", function () {
-  //   searchDown.style.display =
-  //     searchDown.style.display === "none" || searchDown.style.display === ""
-  //       ? "block"
-  //       : "none";
-
-  //   closeBtn.style.display =
-  //     closeBtn.style.display === "none" || closeBtn.style.display === ""
-  //       ? "block"
-  //       : "none";
-  // });
   searchButton.addEventListener("click", function () {
-    searchDown.style.display = "block";
-    closeBtn.style.display = "block";
+    searchDown.style.display =
+      searchDown.style.display === "none" || searchDown.style.display === ""
+        ? "block"
+        : "none";
+    closeBtn.style.display =
+      closeBtn.style.display === "none" || closeBtn.style.display === ""
+        ? "block"
+        : "none";
   });
+
   closeBtn.addEventListener("click", function () {
     searchDown.style.display = "none";
     closeBtn.style.display = "none";
   });
-
-  // window.addEventListener("scroll", function () {
-  //   var scrollPosition = window.scrollY;
-
-  //   // 스크롤 위치가 header의 하단을 넘어가면
-  //   if (scrollPosition > headerHeight) {
-  //     submenu.style.top = "0";
-  //   } else {
-  //     // 스크롤 위치가 header의 하단 이하면
-  //     submenu.style.top = headerHeight - scrollPosition + "px";
-  //   }
-  // });
 });
