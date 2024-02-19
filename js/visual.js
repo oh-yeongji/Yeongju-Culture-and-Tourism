9fa810f19facf5416badaf0c81380e55050a93b0
dev.cappp <dev.cappp@gmail.com>
2024-02-19 20:27:22 +0900

    design: visual text , after css 작업

---
+++ js/visual.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // 페이지 로드 후 실행될 코드
+
+  // 요소 선택
+  var progressBar = document.querySelector(".count .progressBar .bar");
+
+  // 클래스 추가
+  progressBar.classList.add("on");
+});
