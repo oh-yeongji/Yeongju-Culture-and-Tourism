80ac142942a06fda2e87f5975fecf45248492523
dev.cappp <dev.cappp@gmail.com>
2024-02-22 23:40:46 +0900

    design: hotspot css 기초작업 중

--- js/visual.js
+++ js/visual.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // 슬라이드 요소 가져오기
   var slides = document.querySelectorAll(".slides li");
   var currentSlide = 0;
   var slideInterval = setInterval(nextSlide, 2000); // 2초마다 슬라이드 전환
   var stop = document.querySelector(".stop");
   var play = document.querySelector(".play");
 
+  // progressBar 요소 선택
+  const progressBar = document.querySelector(".progressBar");
+  const nowPage = document.querySelector(".nowPage strong");
+
+  // 애니메이션 종료 이벤트 설정
+  progressBar.addEventListener("animationend", () => {
+    // progressBar가 애니메이션을 완료할 때
+    if (progressBar.style.width === "100%") {
+      // nowPage의 내용을 2로 변경
+      
+    }
+  });
 
   function nextSlide() {
     slides[currentSlide].classList.remove("active");
@@ -19,26 +30,22 @@ document.addEventListener("DOMContentLoaded", function () {
     slides[currentSlide].classList.add("active");
   }
 
-  // "prev" 버튼 클릭 시 이전 슬라이드로 이동
   document.querySelector(".prev").addEventListener("click", function () {
     clearInterval(slideInterval);
     prevSlide();
   });
 
-  // "next" 버튼 클릭 시 다음 슬라이드로 이동
   document.querySelector(".next").addEventListener("click", function () {
     clearInterval(slideInterval);
     nextSlide();
   });
 
-  // "stop" 버튼 클릭 시 슬라이드 쇼 일시 중지
   stop.addEventListener("click", function () {
     stop.style.backgroundPosition = "66.66% 0";
     play.style.display = "block";
     clearInterval(slideInterval);
   });
 
-  // "play" 버튼 클릭 시 슬라이드 쇼 재개
   play.addEventListener("click", function () {
     slideInterval = setInterval(nextSlide, 2000);
   });
