2272b0a244e52bdcf689e375dec5bc09dc54fa8c
dev.cappp <dev.cappp@gmail.com>
2024-05-03 23:56:25 +0900

    fix: hotspot slide 기능 추가 초기작업중

---
+++ js/hotspot.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  $(".slidesShow").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
