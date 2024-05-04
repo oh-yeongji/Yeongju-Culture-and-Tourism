6b55ba71b0be0404c20c6d1b830f82b09418a429
dev.cappp <dev.cappp@gmail.com>
2024-05-04 23:56:37 +0900

    fix: slide기능 추가 작업 중

--- js/hotspot.js
+++ js/hotspot.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  $(".slidesShow").slick({
+  $(".slidesWrapper").slick({
     slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    nextArrow: $("slidesShow .btns .right"),
+    prevArrow: $("slidesShow .btns .left"),
   });
 });
