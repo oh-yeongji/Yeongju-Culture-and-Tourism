dc088ab3ab247d217e7b45c25d606725742d3d32
dev.cappp <dev.cappp@gmail.com>
2024-05-07 23:46:18 +0900

    design: hotspot slide image 작업

--- js/hotspot.js
+++ js/hotspot.js
@@ -5,15 +5,14 @@ $(function () {
     slidesToShow: 2, // 한 화면에 보여질 컨텐츠 개수
     slidesToScroll: 1, //스크롤 한번에 움직일 컨텐츠 개수
     speed: 100, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
-    arrows: false, // 옆으로 이동하는 화살표 표시 여부
+    arrows: true, // 옆으로 이동하는 화살표 표시 여부
     dots: false, // 스크롤바 아래 점으로 페이지네이션 여부
     autoplay: false, // 자동 스크롤 사용 여부
     autoplaySpeed: 10000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
     pauseOnHover: false, // 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
     vertical: false, // 세로 방향 슬라이드 옵션
-    prevArrow: $(".prevBtn"), // 이전 화살표 모양 설정
-    nextArrow: "<button type='button' class='slick-next'>Next</button>", // 다음 화살표 모양 설정
-    // dotsClass: "slick-dots", //아래 나오는 페이지네이션(점) css class 지정
+    prevArrow: $(".slick-arrow slick-prev"), // 이전 화살표 모양 설정
+    nextArrow: $(".slick-arrow slick-next"), // 다음 화살표 모양 설정
     draggable: true, //드래그 가능 여부
 
     responsive: [
@@ -35,3 +34,7 @@ $(function () {
     ],
   });
 });
+function changeColor() {
+  var prevBtn = document.querySelector(".prevBtn");
+  prevBtn.style.backgroundColor = "red";
+}
