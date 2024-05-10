afa80a108812de823b31cbe53ec75d2226023c4c
dev.cappp <dev.cappp@gmail.com>
2024-05-10 23:54:21 +0900

    fix: hotspot info btns onclick시 기능 추가 작업

--- js/hotspot.js
+++ js/hotspot.js
@@ -38,3 +38,22 @@ function changeColor() {
   var prevBtn = document.querySelector(".prevBtn");
   prevBtn.style.backgroundColor = "red";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  let nextBtn = document.querySelector(".nextBtn");
+
+  nextBtn.addEventListener("click", function () {
+    let info = document.querySelector(".info");
+
+    info.innerHTML = `
+                <p>New Info Content</p>
+                <h2>New Heading</h2>
+                <div class="hashTag">
+                    <strong>#</strong>
+                   소수서원 문성공묘
+                </div>
+                <div class="place">New Place</div>
+                <div class="history">New History Content</div>
+            `;
+  });
+});
