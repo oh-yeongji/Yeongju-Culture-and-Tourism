1b028a5468719e295702aae8d4a33c218e211f2e
dev.cappp <dev.cappp@gmail.com>
2024-05-14 23:53:37 +0900

    fix: hotspot slick slide 수정 작업

--- js/hotspot.js
+++ js/hotspot.js
@@ -1,36 +1,27 @@
-$(function () {
-  $(".slide-infor").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: ".slider-nav",
-  });
-  $(".slider-pics").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: ".slider-for",
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true,
+$(Document).ready(function () {
+  $(function () {
+    // hotspot pics
+    $(".slider-pics").slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      prevArrow: $(".prevBtn"),
+      nextArrow: $(".nextBtn"),
+      autoplay: false,
+      fade: false,
+      asNavFor: ".slider-info",
+    });
+
+    //hotspot info
+    $(".slider-info").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: false,
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      fade: true,
+      asNavFor: ".slider-pics",
+    });
   });
 });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   let nextBtn = document.querySelector(".nextBtn");
-
-//   nextBtn.addEventListener("click", function () {
-//     let info = document.querySelector(".info");
-
-//     info.innerHTML = `
-//                 <p>New Info Content</p>
-//                 <h2>New Heading</h2>
-//                 <div class="hashTag">
-//                     <strong>#</strong>
-//                    소수서원 문성공묘
-//                 </div>
-//                 <div class="place">New Place</div>
-//                 <div class="history">New History Content</div>
-//             `;
-//   });
-// });
