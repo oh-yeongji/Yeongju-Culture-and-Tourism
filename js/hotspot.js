7a4ac7f5cb768ced397dcabf58084a4a617711fa
dev.cappp <dev.cappp@gmail.com>
2024-05-25 23:53:46 +0900

    design: slick 제거후 slide css만 다시 작업중

--- js/hotspot.js
+++ js/hotspot.js
@@ -1,29 +1,28 @@
 $(Document).ready(function () {
-  $(function () {
-    //hotspot info
-    $(".slider-place").slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      autoplay: false,
-      draggable: false,
-      dots: false,
-      centerMode: true,
-      focusOnSelect: true,
-      fade: false,
-      asNavFor: ".slider-pics",
-    });
-  });
-
+  // $(function () {
+  //   //hotspot info
+  //   $(".slider-place").slick({
+  //     slidesToShow: 1,
+  //     slidesToScroll: 1,
+  //     arrows: false,
+  //     autoplay: false,
+  //     draggable: false,
+  //     dots: false,
+  //     centerMode: true,
+  //     focusOnSelect: true,
+  //     fade: false,
+  //     asNavFor: ".slider-pics",
+  //   });
+  // });
   // hotspot pics
-  $(".slider-pics").slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    prevArrow: $(".prevBtn"),
-    nextArrow: $(".nextBtn"),
-    autoplay: false,
-    draggable: true,
-    fade: false,
-    asNavFor: ".slider-info",
-  });
+  // $(".slider-pics").slick({
+  //   slidesToShow: 2,
+  //   slidesToScroll: 1,
+  //   prevArrow: $(".prevBtn"),
+  //   nextArrow: $(".nextBtn"),
+  //   autoplay: false,
+  //   draggable: true,
+  //   fade: false,
+  //   asNavFor: ".slider-info",
+  // });
 });
