217b5f192578abf6ee9856af504777bdfd45b5eb
dev.cappp <dev.cappp@gmail.com>
2024-07-04 23:56:37 +0900

    design: sideMenu 하단 css 작업

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let sideMenu = document.querySelector("sideMenu");
+  let sideMenu = document.querySelector(".sideMenu");
   let closeX = document.querySelector(".closeX > i");
+  let dim = document.querySelector(".dim");
 
   closeX.addEventListener("click", function () {
     sideMenu.style.display = "none";
+    dim.style.display = "none";
   });
 });
