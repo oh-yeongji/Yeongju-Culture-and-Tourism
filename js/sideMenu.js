58388662febc2c1c21469644898357ef36c698bd
dev.cappp <dev.cappp@gmail.com>
2024-07-05 23:54:41 +0900

    design: display:none으로 변경 작업 중

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -3,6 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
   let closeX = document.querySelector(".closeX > i");
   let dim = document.querySelector(".dim");
 
+  let sideMenuIcon = document.querySelector(".search .side-menu");
+
+  sideMenuIcon.addEventListener("click", function () {
+    console.log("hi");
+    sideMenu.style.display = "block";
+    dim.style.display = "block";
+  });
+
   closeX.addEventListener("click", function () {
     sideMenu.style.display = "none";
     dim.style.display = "none";
