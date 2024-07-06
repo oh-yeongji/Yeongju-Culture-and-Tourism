bb79e27c340056fbdace588c5b15023443eed7d0
dev.cappp <dev.cappp@gmail.com>
2024-07-06 23:22:30 +0900

    feat: sideMenuIcon 클릭시 sideMenu크기 조절 작업중

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -5,20 +5,18 @@ window.onload = function () {
 
   let sideMenuIcon = document.querySelector(".search .side-menu");
 
+  // sieMenuIcon click시 sideMenu크기조절 (100% or 0) 작동안됨
   sideMenuIcon.addEventListener("click", function () {
     console.log("hi");
     sideMenu.style.display =
       sideMenu.style.display === "none" || sideMenu.style.display === ""
         ? "block"
         : "none";
-    dim.style.display =
-      dim.style.display === "none" || dim.style.display === ""
-        ? "block"
-        : "none";
+    dim.style.display = dim.style.width === "0" ? "100%" : "0";
   });
 
-  closeX.addEventListener("click", function () {
-    sideMenu.style.display = "none";
-    dim.style.display = "none";
-  });
+  // closeX.addEventListener("click", function () {
+  //   sideMenu.style.display = "none";
+  //   dim.style.display = "none";
+  // });
 };
