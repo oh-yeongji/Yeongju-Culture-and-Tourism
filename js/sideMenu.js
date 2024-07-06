9f0451dd34f89c13cb95d9aa1429a4cc6f398a09
dev.cappp <dev.cappp@gmail.com>
2024-07-06 23:14:37 +0900

    design: 반응형 웹 크기에 따른 sideMenu 처리작업 중

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.onload = function () {
   let sideMenu = document.querySelector(".sideMenu");
   let closeX = document.querySelector(".closeX > i");
   let dim = document.querySelector(".dim");
@@ -7,12 +7,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
   sideMenuIcon.addEventListener("click", function () {
     console.log("hi");
-    sideMenu.style.display = "block";
-    dim.style.display = "block";
+    sideMenu.style.display =
+      sideMenu.style.display === "none" || sideMenu.style.display === ""
+        ? "block"
+        : "none";
+    dim.style.display =
+      dim.style.display === "none" || dim.style.display === ""
+        ? "block"
+        : "none";
   });
 
   closeX.addEventListener("click", function () {
     sideMenu.style.display = "none";
     dim.style.display = "none";
   });
-});
+};
