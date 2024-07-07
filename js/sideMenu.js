74cf22ad0cf6246baed4c7c95734fb2d440e0023
dev.cappp <dev.cappp@gmail.com>
2024-07-08 01:48:28 +0900

    fix: 전체화면 시 sideMenu click 동작 정상화

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -2,16 +2,12 @@ window.onload = function () {
   let sideMenu = document.querySelector(".sideMenu");
   let closeX = document.querySelector(".closeX > i");
   let dim = document.querySelector(".dim");
-
   let sideMenuIcon = document.querySelector(".side-menu");
 
-
   sideMenuIcon.addEventListener("click", function () {
-    sideMenu.style.display =
-      sideMenu.style.display === "none" || sideMenu.style.display === ""
-        ? "block"
-        : "none";
-    dim.style.display = dim.style.width === "0" ? "100%" : "0";
+    let isHidden = sideMenu.style.display === "none";
+    sideMenu.style.display = isHidden ? "block" : "none";
+    dim.style.display = isHidden ? "block" : "none";
   });
 
   closeX.addEventListener("click", function () {
