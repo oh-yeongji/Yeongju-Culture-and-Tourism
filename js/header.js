1f9863fcbb65859eb380287ce855f77234cf9426
dev.cappp <dev.cappp@gmail.com>
2024-07-11 23:47:52 +0900

    fix: header.js 수정

--- js/header.js
+++ js/header.js
@@ -1,21 +1,50 @@
-window.onload = function () {
-  let sideMenu = document.querySelector(".sideMenu");
-  let closeX = document.querySelector(".closeX > i");
-  let dim = document.querySelector(".dim");
-  let sideMenuIcon = document.querySelector(".side-menu-icon");
+document.addEventListener("DOMContentLoaded", function () {
+  const mainMenuItems = document.querySelectorAll(".mainMenu > li");
+  const subMenus = document.querySelectorAll(".subMenu");
+  const searchButton = document.querySelector(".search");
+  const searchDown = document.getElementById("searchDown");
+  const closeBtn = document.querySelector(".closeBtn");
 
-  sideMenuIcon.addEventListener("click", function () {
-    sideMenu.classList.toggle("open");
-    dim.style.display = sideMenu.classList.contains("open") ? "block" : "none";
+  mainMenuItems.forEach((menuItem, index) => {
+    menuItem.addEventListener("mouseover", function () {
+      subMenus.forEach((submenu, subIndex) => {
+        if (subIndex === index) {
+          submenu.style.height = "378px";
+          submenu.style.display = "block";
+          const h2 = submenu.querySelector("h2");
+          if (h2 && h2.textContent.includes("여행정보")) {
+            submenu.style.height = "500px";
+          }
+        } else {
+          submenu.style.height = "0";
+          submenu.style.display = "none";
+        }
+      });
+    });
   });
 
-  closeX.addEventListener("click", function () {
-    sideMenu.classList.remove("open");
-    dim.style.display = "none";
+ 
+  subMenus.forEach((submenu) => {
+  
+    submenu.addEventListener("mouseleave", () => {
+
+      submenu.style.display = "none";
+    });
   });
 
-  dim.addEventListener("click", function () {
-    sideMenu.classList.remove("open");
-    dim.style.display = "none";
+  searchButton.addEventListener("click", function () {
+    searchDown.style.display =
+      searchDown.style.display === "none" || searchDown.style.display === ""
+        ? "block"
+        : "none";
+    closeBtn.style.display =
+      closeBtn.style.display === "none" || closeBtn.style.display === ""
+        ? "block"
+        : "none";
   });
-};
+
+  closeBtn.addEventListener("click", function () {
+    searchDown.style.display = "none";
+    closeBtn.style.display = "none";
+  });
+});
