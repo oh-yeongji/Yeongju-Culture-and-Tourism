0df4c89f1928841f70de0b2178cc66714a459063
dev.cappp <dev.cappp@gmail.com>
2024-07-17 23:48:01 +0900

    feat:sideMenu smallLi animation기능 적용초기

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -1,8 +1,14 @@
 window.onload = function () {
   let sideMenu = document.querySelector(".sideMenu");
   let closeX = document.querySelector(".closeX > i");
-  let dim = document.querySelector(".dim");
   let sideMenuIcon = document.querySelector(".side-menu");
+  let dim = document.querySelector(".dim");
+  let sideMenuBigLi = document.querySelectorAll(
+    ".sideMenu .sideBody > li > a"
+  );
+  let sideMenuMediLi = document.querySelectorAll(
+    ".sideMenu .sideBody > li > ul"
+  );
 
   sideMenuIcon.addEventListener("click", function () {
     sideMenu.classList.toggle("show");
@@ -17,5 +23,20 @@ window.onload = function () {
   dim.addEventListener("click", function () {
     sideMenu.classList.remove("show");
     dim.style.display = "none";
+  });
+
+  sideMenuBigLi.forEach(function (menu) {
+    menu.addEventListener("click", function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      let subMenu = this.nextElementSibling;
+      if (subMenu) {
+        let isExpanded = subMenu.style.display === "block";
+        sideMenuMediLi.forEach(function (submenu) {
+          submenu.style.display = "none";
+        });
+        subMenu.style.display = isExpanded ? "none" : "block";
+      }
+    });
   });
 };
