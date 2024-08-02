673d849b47e1b3bb59bc653471a523ecc6bb4766
dev.cappp <dev.cappp@gmail.com>
2024-08-02 23:43:24 +0900

    refactor: submenu bigLi 한개씩만 열리는코드 수정
    sideMenuMediLl변수를 querySelectorAll에서 a태그까지 했을때
    3줄의 코드를 추가한것을 ul태그까지만으로 수정하고 코드줄임

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -5,68 +5,58 @@ window.onload = function () {
   let dim = document.querySelector(".dim");
   let sideMenuBigLi = document.querySelectorAll(".sideMenu .sideBody > li > a");
   let sideMenuMediLl = document.querySelectorAll(
-    ".sideMenu .sideBody > li > ul > li > a "
+    ".sideMenu .sideBody > li > ul"
   );
   let sideMenuSmalLl = document.querySelectorAll(
     ".sideMenu .sideBody > li > ul > li > ul"
   );
-
   sideMenuIcon.addEventListener("click", function () {
     sideMenu.classList.toggle("show");
     dim.style.display = sideMenu.classList.contains("show") ? "block" : "none";
   });
-
   closeX.addEventListener("click", function () {
     sideMenu.classList.remove("show");
     dim.style.display = "none";
   });
-
   dim.addEventListener("click", function () {
     sideMenu.classList.remove("show");
     dim.style.display = "none";
   });
 
-  // bigLi에 forEach돌려서 클릭하면 전파방지,a태그기본동작중지, this에 자매본다
   sideMenuBigLi.forEach(function (menu) {
     menu.addEventListener("click", function (event) {
       event.preventDefault();
       event.stopPropagation();
       let subMenu = this.nextElementSibling;
       if (subMenu) {
-        // 클릭된 메뉴 외의 다른 메뉴의 하위 메뉴를 모두 닫기
-        sideMenuBigLi.forEach(function (otherMenu) {
-          if (otherMenu !== menu && otherMenu.nextElementSibling) {
-            otherMenu.nextElementSibling.classList.remove("show");
-          }
-        });
-        // 하위 메뉴가 열려 있는지 확인
         let isExpanded = subMenu.classList.contains("show");
-        // 중간 메뉴 항목에서 show 클래스를 모두 제거
         sideMenuMediLl.forEach(function (submenu) {
           submenu.classList.remove("show");
         });
-        // 현재 하위 메뉴의 show 클래스를 반대로 설정
         subMenu.classList.toggle("show", !isExpanded);
       }
     });
   });
 
-  // mediLi를 foreach돌려서 각각을 클릭할때 전파방지 , a태그 기본동작 방지 걸어주고 biLi자매보고
   sideMenuMediLl.forEach(function (menu) {
     menu.addEventListener("click", function (event) {
       event.preventDefault();
       event.stopPropagation();
       let subMenu = this.nextElementSibling;
+
       if (subMenu) {
-        // biLi자매한테 show있는지 확인
         let isExpanded = subMenu.classList.contains("show");
-        // smalLi forEach 톨려서 show제거하고
         sideMenuSmalLl.forEach(function (submenu) {
           submenu.classList.remove("show");
         });
-        // mediLi에 show를 강제로 반대
-        // 이걸주석처리하니까 mediLi가 안열림
         subMenu.classList.toggle("show", !isExpanded);
+
+        if (isExpanded) {
+          subMenu.querySelectorAll("li > a").forEach(function (a) {
+            a.style.background = "#fff";
+            a.style.borderBottom = "none";
+          });
+        }
       }
     });
   });
